8f210ce671c5c6c8dbd8faf72af45f05fafe005a
prabashwara65 <prabashwara65@gmail.com>
2024-07-01 22:54:37 +0530

    commit

--- src/app/(sub pages)/contact/page.js
+++ src/app/(sub pages)/contact/page.js
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import bg from "../../../../public/background/contact-background.png"
+import Navigation from "../../Components/navigation";
 
 
 // import Macbook from "./Components/models/Macbook";
@@ -7,10 +8,34 @@ import bg from "../../../../public/background/contact-background.png"
 export default function Home() {
     return (
         <>
-            <Image 
-            src={bg} 
-            alt="background-image" 
-            fill className="w-full h-full object-cover object-center opacity-25" />
+            <Image
+                src={bg}
+                alt="background-image"
+                fill className="w-full h-full object-cover object-center opacity-25" />
+
+            <div className="flex items-center justify-center fixed top-0 left-10 h-screen z-10">
+
+                <Navigation />
+
+                {/* <RenderModel>
+                    <Staff />
+                </RenderModel> */}
+            </div>
+
+            <article className="relative w-full flex flex-col items-center justify-center space-y-8">
+
+                <div className="flex flex-col items-center justify-center space-y-6 w-3/4">
+                <h1 className="text-accent font-semibold text-center text-4xl capitalize">
+                        Summon the wizard
+                </h1>
+                <p className="text-center font-light">
+                    lorem jasdkasfaif njefjsefkjs ke sfjefnskms efeahfke fs efaeifaefa ryaea raihfae fakenfoiaef aefa efka
+                    lorem jasdkasfaif njefjsefkjs ke sfjefnskms efeahfke fs efaeifaefa ryaea raihfae fakenfoiaef aefa efka
+                </p>
+
+                </div>
+
+            </article>
 
         </>
     );
