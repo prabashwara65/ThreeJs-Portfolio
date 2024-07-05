470ac7e5487f618af268c371897138339634b5ed
prabashwara65 <prabashwara65@gmail.com>
2024-07-05 23:30:25 +0530

    commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,11 +18,12 @@ module.exports = {
         muted: "rgb(var(--muted))",
         accent: "rgb(var(--accent))",
 
+      },  
+                              
+      backgroundImage: {
+        'purple-radial': "radial-gradient(50% 50% at 50% 50%, rgba(159, 0, 255, 0.5) 0%, rgba(159, 0, 255, 0) 100%)"
       },
       
-      backgroundImage:{
-        'firefly-radial': "radial-gradient(50% 50% at 50% 50%, rgba(253, 255, 80, 0.5) 0%, rgba(217,217,217, 0) 100%)"
-      },
 
       boxShadow: {
         'glass-inset' : 'inset 0 17px 5px -9px rgba(254,254,91, 0.05)',
