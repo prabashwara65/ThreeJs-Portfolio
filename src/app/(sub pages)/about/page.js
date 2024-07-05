fa22bbb930fb9d34b53e129de77e6abb8e2ddfad
prabashwara65 <prabashwara65@gmail.com>
2024-07-05 23:05:02 +0530

    commit about

--- src/app/(sub pages)/about/page.js
+++ src/app/(sub pages)/about/page.js
@@ -27,9 +27,9 @@ export default function Home() {
 
 
             <div className="relative w-full h-screen flex flex-col items-center justify-center">
-                <div className="absolute flex flex-col items-center text-center top-1/2 sm:top-[60%] left-1/2 -translate-x-1/2">
-                    <h1 className="font-bold text-6xl xs:text-7xl sm:text-8xl lg:text-9xl text-accent">Prabashwara</h1>
-                    <p className="font-light text-foreground text-ls">Meet the wizard behind this portfolio</p>
+                <div className="absolute flex flex-col items-center text-center top-1/2 sm:top-[50%] left-1/2 -translate-x-1/2">
+                    <h1 className="font-bold text-6xl xs:text-7xl sm:text-8xl lg:text-9xl text-accent">Priyanka Prabashwara</h1>
+                    <p className="font-light text-foreground text-ls">Full Stack Dev / Writer </p>
                 </div>
             </div>
 
